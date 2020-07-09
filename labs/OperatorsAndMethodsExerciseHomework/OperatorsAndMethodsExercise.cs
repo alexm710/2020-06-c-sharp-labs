f4ced8ef4f9178cdee783707f4ad646bb4b5b521
alexm710 <alexm208@gmail.com>
2020-07-09 21:01:24 +0100

    Adding completed homework

---
+++ labs/OperatorsAndMethodsExerciseHomework/OperatorsAndMethodsExercise.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OperatorAndMethodsExercisesLib
+{
+    public class Methods
+    {
+        // implement this method so that it returns true if x is greater than or equal to y
+        public static bool GreaterEqual(int x, int y)
+        {
+            return false;
+        }
+
+        // implement this method so that it returns the number of dozens of eggs
+        public static int Dozens(int numEggs)
+        {
+            return -1;
+        }
+
+        // Implement this method so that it take a double as an input,
+        // squares it, adds 101, divides the result by 7, then subtracts 4.  
+        // Return a double rounded to 3 decimal places.
+        public static double BODMAS(int inputNumber)
+        {
+         
+            return -1;
+        }
+
+        // implement this method so that it returns the sum of x and y
+        // and sets the out parameter 'product' to be the product of x and y
+        public static int SumProduct(int x, int y, out int product)
+        {
+           
+            product = -1;
+            return -1;
+        }
+
+        // implement this method so it returns a tuple (weeks, days) 
+        // corresponding to a given number of days
+        public static (int weeks, int days) DaysAndWeeks(int totalDays)
+        {
+
+            return (-1, -1);
+        }
+    }
+}
