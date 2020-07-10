06acfe49e54599081f19996da9d99f816e0bc2f6
alexm710 <alexm208@gmail.com>
2020-07-10 09:23:40 +0100

    Will be updated soon

--- labs/lab_04_wpf_core/MainWindow.xaml.cs
+++ labs/lab_04_wpf_core/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,132 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Calculator
+namespace lab_04_wpf_core
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            // windows command
-            InitializeComponent();
-            LabelDisplay.Content = "";
-        }
-
-        static int firstNumber = 0;
-        static int secondNumber1 = 0;
-        static string operation = "";
-
-        int getAnswer()
-        {
-            switch (operation)
-            {
-                case "*": return firstNumber * secondNumber1;
-                case "/": return firstNumber / secondNumber1;
-                case "+": return firstNumber + secondNumber1;
-                case "-": return firstNumber - secondNumber1;
-                default: return Convert.ToInt32(LabelDisplay.Content);
-            }
-        }
-
-        private void Button01_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button01.Content;
-        }
-
-        private void Button02_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button02.Content;
-        }
-
-        private void Button03_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button03.Content;
-        }
-
-        private void Button04_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button04.Content;
-        }
-
-        private void Button05_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button05.Content;
-        }
-
-        private void Button06_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button06.Content;
-        }
-
-        private void Button07_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button07.Content;
-        }
-
-        private void Button08_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button08.Content;
-        }
-
-        private void Button09_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button09.Content;
-        }
-
-        private void Button00_Click(object sender, RoutedEventArgs e)
-        {
-            LabelDisplay.Content += (string)Button00.Content;
-        }
-
-        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "+";
-            firstNumber = Convert.ToInt32(LabelDisplay.Content);
-            LabelDisplay.Content = "";
-        }
-
-        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "-";
-            firstNumber = Convert.ToInt32(LabelDisplay.Content);
-            LabelDisplay.Content = "";
-        }
-
-        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "*";
-            firstNumber = Convert.ToInt32(LabelDisplay.Content);
-            LabelDisplay.Content = "";
-        }
-
-        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "/";
-            firstNumber = Convert.ToInt32(LabelDisplay.Content);
-            LabelDisplay.Content = "";
-        }
-
-        private void ButtonEquals_Click(object sender, RoutedEventArgs e)
-        {
-            secondNumber1 = Convert.ToInt32(LabelDisplay.Content);
-            int ans = getAnswer();
-            string answer = ans.ToString();
-            LabelDisplay.Content = answer;
-
-            firstNumber = 0;
-            secondNumber1 = 0;
-            operation = "";
-        }
-
-        private void ButtonAC_Click(object sender, RoutedEventArgs e)
-        {
-            firstNumber = 0;
-            secondNumber1 = 0;
-            operation = "";
-            LabelDisplay.Content = "";
-        }
     }
-}
