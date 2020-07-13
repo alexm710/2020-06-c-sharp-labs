c885a2a519c6b1078affc72a90f0823b7f11320b
alexm710 <alexm208@gmail.com>
2020-07-13 17:56:35 +0100

    Adding completed homework

---
+++ labs_homework/IterationExercises/IterationExercises/Highest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IterationLib
+{
+    public class Highest
+    {
+        public static int HighestWhileLoop(int[] nums)
+        {
+            int maxNum = nums[0];
+            int i = 1;
+            while (i < nums.Length)
+            {
+                if (nums[i] > maxNum) maxNum = nums[i];
+                i++;
+            }
+            return maxNum;
+        }
+    }
+
+        public static int HighestForLoop(int[] nums)
+        {
+            // this method should use a for loop
+            throw new NotImplementedException();
+        }
+
+        public static int HighestForEachLoop(int[] nums)
+        {
+            // this method should use a for-each loop
+            throw new NotImplementedException();
+        }
+
+        public static int HighestDoWhileLoop(int[] nums)
+        {
+            // this method should use a do-while loop
+            throw new NotImplementedException();
+        }
+    }
+}
